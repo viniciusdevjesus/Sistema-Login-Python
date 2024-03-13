315bebe9870c2d210c3ab1cd8d8b70f5fc4fd489
viniciusdevjesus <viniciusjesusfilho07@gmail.com>
2024-03-12 22:55:29 -0300

    fix: bug_sqlQuery

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
-from Window import Email_name
-from Window import Password_name
+# from Window import Email_name
+# from Window import Password_name
 import mysql.connector
 conect = mysql.connector.connect(
     host='localhost',
@@ -7,45 +7,35 @@ conect = mysql.connector.connect(
     password='',
     database='BD_CLI'
 )
-cursor = conect.cursor()
+cursor = conect.cursor(buffered=True)
 logName = str(input("Digite seu email: "))
 logPassword = str(input("Digite sua senha: "))
-comando = "SELECT '{}' FROM Clientes".format(logName)
-cursor.execute(comando)
-resultado = cursor.fetchall()
+
+# comando = "SELECT '{}' FROM Clientes".format(logName)
+# cursor.execute(comando)
+# resultado = cursor.fetchall()
 
 
 def Login():
-    conect = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password='',
-    database='BD_CLI'
-    )
-    cursor = conect.cursor()
-    comando1 = "SELECT '{}' FROM Clientes".format(Email_name)
-    comando2 = "SELECT '{}' FROM Clientes".format(Password_name)
+    comando1 = "SELECT * FROM Clientes WHERE nome= '{}'".format(logName)
+    comando2 = "SELECT * FROM Clientes WHERE senha= '{}'".format(logPassword)
     cursor.execute(comando1)
-    cursor.execute(comando2)
     resultado1 = cursor.fetchall()
+    cursor.execute(comando2)
     resultado2 = cursor.fetchall()
-    if resultado == Email_name:
-        print ("ok")
+    for row in resultado1:
+        if logName in row:
+            print("acertou o email")
+        else:
+            print("Eroou o email")
+    for row in resultado2:
+        if logPassword in row:
+            print("acertou a senha")
+        else:
+            print("Eroou a senha")
 
 
-
-# print("========Sistema de Cadastro de Clientes=======")
-# decision = int(input("[1] - Login\n[2] - Cadastro\n"))
-# if decision == 1:
-#     logName = str(input("Digite seu email: "))
-#     logPassword = str(input("Digite sua senha: "))
-#     comando = 'SELECT {logName} FROM Clientes'
-#     cursor.execute(comando)
-#     resultado = cursor.fetchall()
-#     if resultado != logName:
-#         print("Login incorreto")
-
-
+print(Login())
 # # comando = 'INSERT INTO(nome,numero,email,senha) VALUES ()'
 # # cursor.execute(comando)
 # # conect.commit() # edita o banco de dados 
